3c4a903e85656bbd3678e01f1582175115aea75c
Anthony Pyrtle <xxpyrtlexx93@gmail.com>
2015-03-27 16:50:03 -0400

    Update tesla.mk

--- tesla.mk
+++ tesla.mk
@@ -6,4 +6,6 @@ $(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
 # Inherit some common Tesla stuff.
 $(call inherit-product, vendor/tesla/config/common.mk)
 
+PRODUCT_DEVICE := m8
+
 PRODUCT_NAME := tesla_m8
