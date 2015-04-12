76094c1758f74a9b7e3b98f59da41dafff1ea08e
Anthony Pyrtle <xxpyrtlexx93@gmail.com>
2015-04-11 22:43:18 -0400

    remove

--- tesla.mk
+++ tesla.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/htc/m8/full_m8.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
+#$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
 
 # Inherit some common Tesla stuff.
 $(call inherit-product, vendor/tesla/config/common.mk)
